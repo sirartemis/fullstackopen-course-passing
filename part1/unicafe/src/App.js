d70a389a519d5c327b8677aa746526cd98834529
Artem <sirartemis91@gmail.com>
2022-08-21 14:24:37 +0300

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,7 +18,7 @@ const Statistics = ({ good, neutral, bad }) => {
           <StatisticLine text='good' value={good} />
           <StatisticLine text='neutral' value={neutral} />
           <StatisticLine text='bad' value={bad} />
-          <StatisticLine text='all' value={all()} />
+          <StatisticLine text='all' value={all} />
           <StatisticLine text='average' value={average()} />
           <StatisticLine text='positive' value={`${positive()}%`} />
         </tbody>
